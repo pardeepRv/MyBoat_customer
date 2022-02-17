0298160aaa2257faafe9a9172c5a754c100766e5
iosexperts <iosexperts@rvtechnologies.com>
2022-02-17 18:40:16 +0530

    notification integrated

--- App.js
+++ App.js
@@ -1,33 +1,44 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
 import { Root } from "native-base";
-import TabNav from './src/Navi/BottomNav';
+import TabNav from "./src/Navi/BottomNav";
 // import  from './src/Navi/BottomNav';
-import Stacks from './src/Navi/Stack';
+import Stacks from "./src/Navi/Stack";
 
 //import all the components we are going to use
-import { View, LogBox, StatusBar  , SafeAreaView} from "react-native";
-import { Colors } from './src/Constants/Constants';
+import { View, LogBox, StatusBar, SafeAreaView } from "react-native";
+import { Colors } from "./src/Constants/Constants";
+import { requestUserPermission } from "./src/service/FcmService";
+import { createNotificationListener } from "./src/service/notificationListener";
+import checkPermission from "./src/service/notificationServices";
 
 LogBox.ignoreAllLogs();
 
-const App =()=>{
-  return(
+const App = () => {
+  
+  useEffect(() => {
+    checkPermission();
+    requestUserPermission();
+    createNotificationListener();
+  }, []);
+  
+  return (
     <NavigationContainer>
-     
-            <SafeAreaView style={{
-              backgroundColor: Colors.orange
-            }} />
-            {/* <StatusBar hidden /> */}
-            <StatusBar backgroundColor={Colors.orange} />
-            <Stacks />
-            {/* <TabNav /> */}
-        
+      <SafeAreaView
+        style={{
+          backgroundColor: Colors.orange,
+        }}
+      />
+      {/* <StatusBar hidden /> */}
+      <StatusBar backgroundColor={Colors.orange} />
+      <Stacks />
+      {/* <TabNav /> */}
+
       {/* <StatusBar hidden />
       <Stacks /> */}
       {/* <TabNav /> */}
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
